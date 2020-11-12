44c299ad1d939cddac147f64851cd15cd6800f76
yanalorien <yankaeva@gmail.com>
2020-11-11 23:20:06 -0500

    Test Suit Pack runner

---
+++ MySuite/src/test/java/pack1/Test1.java
@@ -0,0 +1,14 @@
+package pack1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test1
+{
+	@Test
+	void test()
+	{
+		System.out.println("Test1");
+	}
+}
---
+++ MySuite/src/test/java/pack1/Test2.java
@@ -0,0 +1,14 @@
+package pack1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test2
+{
+	@Test
+	void test()
+	{
+		System.out.println("Test2");
+	}
+}
