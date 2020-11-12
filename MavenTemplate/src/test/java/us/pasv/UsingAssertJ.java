b31c0b1ca01d30ea6081010186048cf452281ba6
yanalorien <yankaeva@gmail.com>
2020-11-11 23:02:31 -0500

    soft assertions

--- MavenTemplate/src/test/java/us/pasv/UsingAssertJ.java
+++ MavenTemplate/src/test/java/us/pasv/UsingAssertJ.java
@@ -1,16 +1,28 @@
 package us.pasv;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 class UsingAssertJ
 {
+	private Map<String, Integer> map = Map.of("One", 1, "Two", 2, "Three", 3);
 	private List<String> list = Arrays.asList("1", "2", "3");
+	private String name = "Vladimir";
+	
+	@Test
+	void mapTest()
+	{
+		assertThat(map)
+			.isNotEmpty()
+			.doesNotContainKey("Ten")
+			.contains(entry("Three", 3));
+	}
 	
 	@Test
 	void listTest()
@@ -21,4 +33,29 @@ class UsingAssertJ
 			.containsSequence("2", "3")
 			.doesNotContain("13");
 	}
+	
+	@Test
+	void stringTest()
+	{
+		assertThat(name).isEqualTo("Vladimir");
+		assertThat(name).as("testing name").isNotEqualTo("Vlad");
+	}
+	
+	@Test
+	void softTest()
+	{
+		SoftAssertions softly = new SoftAssertions();
+		softly.assertThat(name)
+			.startsWith("Tony")
+			.contains("B");
+		
+		softly.assertAll();
+	}
 }
+
+
+
+
+
+
+
