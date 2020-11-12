4474aa9a5dd8fd9abe04757fc7b702017bbd08ea
yanalorien <yankaeva@gmail.com>
2020-11-12 00:38:35 -0500

    parameterized tests beginning

---
+++ MavenTemplate/src/test/java/us/pasv/ParameterizedTests.java
@@ -0,0 +1,14 @@
+package us.pasv;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ParameterizedTests
+{
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+}
