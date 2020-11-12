a0eb8aac56e200f52562c0e7e4f210cd036f10f9
yanalorien <yankaeva@gmail.com>
2020-11-11 23:28:11 -0500

    @selectPackages run packages

---
+++ MySuite/src/test/java/pack2/TestSuitePack2.java
@@ -0,0 +1,10 @@
+package pack2;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages({"pack1"})
+class TestSuitePack2
+{}
