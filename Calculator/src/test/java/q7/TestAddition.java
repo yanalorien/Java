c973b12bc47ecd3fff6acd86fc78ec889abd6fdd
yanalorien <yankaeva@gmail.com>
2020-11-18 22:50:00 -0500

    calculator homework

--- Calculator/src/test/java/q7/TestAddition.java
+++ Calculator/src/test/java/q7/TestAddition.java
@@ -2,17 +2,33 @@ package q7;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestAddition
 {
+	private Calculator calc;
+	
+	@BeforeEach
+	void setUp()
+	{
+		calc = new Calculator();
+		calc.switchOn();
+	}
+	
 	@Test
 	void test()
 	{
-		Calculator calc = new Calculator();
-		calc.switchOn();
 		calc.addition(1);
 		calc.addition(3);
 		assertEquals(4, calc.getResult());
+	}
+	
+	@Test
+	void test2()
+	{
+		calc.addition(100);
+		calc.addition(100);
+		assertEquals(200, calc.getResult());
 	}
 }
