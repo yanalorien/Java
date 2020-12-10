108108b19528348a55aac46b1c8490d92ab77ce1
yanalorien <yankaeva@gmail.com>
2020-12-10 00:00:34 -0500

    find elements in table

--- Fundamentals/src/test/java/tests/table/tables.java
+++ Fundamentals/src/test/java/tests/table/tables.java
@@ -32,5 +32,18 @@ class tables extends BaseTest
 			String[] rowText = row.getText().split(" ");
 			System.out.println(rowText[3] + " " + rowText[4]);
 		}
+		
+		System.out.println("\nSecond method");
+		String cellXpath = "";
+		for(int i = 1; i <= numberOfRows; i++)
+		{
+			cellXpath = "//table[2]/tbody/tr[" + i + "]/td[4]";
+			System.out.println(driver.findElement(By.xpath(cellXpath)).getText());
+		}
+		
+		System.out.println("\nThird method");
+		List<WebElement> column = driver.findElements(By.xpath("//td[@class = 'dues']"));
+		for (WebElement cell: column)
+			System.out.println(cell.getText());
 	}
 }
