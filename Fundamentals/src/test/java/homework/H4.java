c0744aea15dc62fb8acee7feba4a09d1550734b3
yanalorien <yankaeva@gmail.com>
2020-12-14 23:21:59 -0500

    hw4 get column values

---
+++ Fundamentals/src/test/java/homework/H4.java
@@ -0,0 +1,44 @@
+package homework;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import common.BaseTest;
+
+class H4 extends BaseTest
+{
+	private String givenColumnName = "Phone";
+	@BeforeEach
+	void open() throws Exception
+	{
+		driver.get("http://demo.automationtesting.in/WebTable.html");
+	}
+
+	@Test
+	void test()
+	{
+		int givenColumn = 0;
+		int currentColumn = 1;
+		
+		System.out.println("Column headers:");
+		List<WebElement> headers = driver.findElements(By.className("ui-grid-header-cell-label"));
+
+		for (WebElement header : headers)
+		{
+			String columnName = header.getText();
+			System.out.println(columnName);
+			if (columnName.contains(givenColumnName))
+				givenColumn = currentColumn;
+			else
+				currentColumn++;
+		}
+		
+		System.out.println("\nGiven " + givenColumnName + " number is: " + givenColumn);
+	}
+}
