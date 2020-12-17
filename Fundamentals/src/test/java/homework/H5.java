6a1b86b88f0e601e481f324e038330811ea6cfac
yanalorien <yankaeva@gmail.com>
2020-12-16 22:52:15 -0500

    hw5

---
+++ Fundamentals/src/test/java/homework/H5.java
@@ -0,0 +1,44 @@
+package homework;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import common.BaseTest;
+
+class H5 extends BaseTest
+{
+	@BeforeEach
+	void open() throws Exception
+	{
+		driver.get("https://www.trademe.co.nz/");
+	}
+
+	@Test
+	void test()
+	{
+		driver.findElement(By.linkText("Services")).click();
+		Select regions = new Select(driver.findElement(By.id("134")));
+		regions.selectByVisibleText("Auckland");
+		
+		Select category = new Select(driver.findElement(By.id("193")));
+		category.selectByVisibleText("Events & entertainment");
+		
+		Select subCategory = new Select(driver.findElement(By.id("209")));
+		subCategory.selectByVisibleText("Photography & video");
+		
+		driver.findElement(By.id("Sidebar-153")).sendKeys("wedding");
+		
+		driver.findElement(By.className("button27")).click();
+		
+		List<WebElement> links = driver.findElements(By.partialLinkText("Wedding"));
+		assertThat(links.size()).isGreaterThan(0);
+	}
+}
