d6e1d8213b98d470fbc2b79f4d7f76919449a79e
yanalorien <yankaeva@gmail.com>
2021-01-27 23:55:50 -0500

    gets with pojo

--- RESTAutomation/src/test/java/tests/Gets.java
+++ RESTAutomation/src/test/java/tests/Gets.java
@@ -1,4 +1,4 @@
-package rest;
+package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
---
+++ RESTAutomation/src/test/java/tests/GetsWithPOJO.java
@@ -0,0 +1,38 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import pages.TimezonePage;
+
+class GetsWithPOJO
+{
+	private WebTestClient client;
+	private String baseURL = "http://worldtimeapi.org/api/timezone";
+
+	@BeforeEach
+	void setUp()
+	{
+		client = WebTestClient.bindToServer().baseUrl(baseURL).build();
+	}
+
+	@Test
+	void test()
+	{
+		EntityExchangeResult<TimezonePage> result = client.get().uri("/America/Los_Angeles")
+					.exchange()
+					.expectStatus().isOk()
+					.expectHeader()
+					.contentType(MediaType.APPLICATION_JSON)
+					.expectBody(TimezonePage.class)
+					.returnResult();
+		
+		TimezonePage response = result.getResponseBody();
+	}
+}
